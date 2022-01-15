e57c408cb601031f0cf50d6975e822db25167b97
CandyJanus <85752963+CandyJanus@users.noreply.github.com>
2022-01-15 15:48:32 -0800

    hurray, it works
    want to add more features, though

--- src/data/campaign/rulecmd/nukePlanet_RULECMD.java
+++ src/data/campaign/rulecmd/nukePlanet_RULECMD.java
@@ -6,13 +6,19 @@ import com.fs.starfarer.api.campaign.StarSystemAPI;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import com.fs.starfarer.api.impl.campaign.intel.deciv.DecivTracker;
+import com.fs.starfarer.api.impl.campaign.procgen.themes.BaseThemeGenerator;
+import com.fs.starfarer.api.impl.campaign.procgen.themes.MiscellaneousThemeGenerator;
 import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
 import com.fs.starfarer.api.util.Misc;
 
 import java.util.List;
 import java.util.Map;
 
-public class nukePlanet extends BaseCommandPlugin {
+public class nukePlanet_RULECMD extends BaseCommandPlugin {
+
+    //note: this constant is an arbitrary number
+
+    int PLANET_DEBRIS_RADIUS=400;
 
     @Override
     public boolean execute(String ruleId,
@@ -36,32 +42,48 @@ public class nukePlanet extends BaseCommandPlugin {
             //note: It does!
             for (SectorEntityToken entity : market.getConnectedEntities()) {
                 system.removeEntity(entity);
+                //note: force deciv, should remove market for real
+                //note: full destroy, full destroy withIntel
             }
+            DecivTracker decivtracker=DecivTracker.getInstance(); //note: wait, this is a static object I think? I shouldn't need to do this???
+            decivtracker.decivilize(market, true, true);
         }
 
-        //note: force deciv, should remove market for real
-            //note: full destroy, full destroy withIntel
+        //note: todo: remove rings and coronas from planet
 
-        DecivTracker decivtracker=DecivTracker.getInstance(); //note: wait, this is a static object I think? I shouldn't need to do this???
-        decivtracker.decivilize(market, true, true);
+        //note: todo remove nascent gravity well
 
         //note: spawn debris around planet
 
-        //todo
+        MiscellaneousThemeGenerator MiscGen = new MiscellaneousThemeGenerator(); //note: The generator doesn't *seem* to be static, though the methods are?
+        BaseThemeGenerator.StarSystemData sysData = MiscGen.computeSystemData(system);
+
+        MiscGen.addDebrisField(sysData, planet, PLANET_DEBRIS_RADIUS); //note: todo could make debris radius dependent on the kind of target, currently it's a constant.
+
+        //note: todo spawn asteroid field around planet
 
         //note: destroy planet
 
         system.removeEntity(planet);
 
-        //note: remove nascent gravity well
+        //note: todo make the explosion effect that happens when a battle is won. I have no idea where to do this.
 
-        //todo
+
+
+
 
         //note: sanity check that planet is gone
 
-        //todo
+        if (planet==null)
+        {
+            return true;
+        }
+        else{
 
-        return true;
+            return false; //note: wait, does returning false throw an exception anywhere, or
+        }
+
+
     }
 }
 
